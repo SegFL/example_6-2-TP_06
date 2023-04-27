a471ec6481322ec3ce66b512a07b1df1f141d61e
SegFL <sfernandezl@fi.uba.ar>
2023-04-27 23:07:43 +0000

    cambios clase 27.04.2023

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 
 //=====[Main function, the program entry point after power on or reset]========
 
-int main()
-{
-    smartHomeSystemInit();
-    while (true) {
-        smartHomeSystemUpdate();
-    }
+int main() {
+  smartHomeSystemInit();
+  while (true) {
+    smartHomeSystemUpdate();
+  }
 }
